71152c3ded4a9b3957240c09054daae79ed75801
Maria Aragao <maria.s.aragao.2025@gmail.com>
2025-08-09 15:03:34 -0300

    chore: Estrutura do Projeto Spring Boot

---
+++ src/main/java/com/maria/paginas_marcadas/PaginasMarcadasApplication.java
@@ -0,0 +1,13 @@
+package com.maria.paginas_marcadas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginasMarcadasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginasMarcadasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maria/paginas_marcadas/PaginasMarcadasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maria.paginas_marcadas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginasMarcadasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
