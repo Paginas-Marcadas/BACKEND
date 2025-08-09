83bd02cfc6cb1cb832c29a0b6cde333c0889cc89
Maria Aragao <maria.s.aragao.2025@gmail.com>
2025-08-09 16:12:41 -0300

    feature: criação entidade tag e o enum Tipo

---
+++ src/main/java/com/maria/paginas_marcadas/entity/Tag.java
@@ -0,0 +1,62 @@
+package com.maria.paginas_marcadas.entity;
+
+import com.maria.paginas_marcadas.entity.enums.TipoTag;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tag")
+public class Tag {
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_tag")
+    private Long idTag;
+
+    @ManyToOne
+    @JoinColumn(name = "id_usuario", nullable = false)
+    private Usuario usuario;
+
+    @Column(name = "nome_tag", nullable = false, length = 100)
+    private String nome;
+    
+    @Column(name = "tipo_tag")
+    @Enumerated(EnumType.STRING)
+    private TipoTag tipo;
+
+    /*
+    @ManyToMany
+    @JoinTable(
+        name = "tag_memoria_leitura",
+        schema = "principal",
+        joinColumns = @JoinColumn(name = "id_tag"),
+        inverseJoinColumns = @JoinColumn(name = "id_memoria_leitura")
+    )
+    private List<MemoriaLeitura> memoriasLeitura = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(
+        name = "tag_memoria_livro",
+        schema = "principal",
+        joinColumns = @JoinColumn(name = "id_tag"),
+        inverseJoinColumns = @JoinColumn(name = "id_memoria_livro")
+    )
+    private List<MemoriaLivro> memoriasLivro = new ArrayList<>();
+    */
+}
