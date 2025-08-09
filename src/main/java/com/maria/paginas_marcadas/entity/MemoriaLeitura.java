d4f7e49f9cb7a49259ea5c87cd1d537cee63478f
Maria Aragao <maria.s.aragao.2025@gmail.com>
2025-08-09 19:36:26 -0300

    feature: criação entidade Memória de Leitura e seu status equivalente

--- src/main/java/com/maria/paginas_marcadas/entity/MemoriaLeitura.java
+++ src/main/java/com/maria/paginas_marcadas/entity/MemoriaLeitura.java
@@ -1,15 +1,13 @@
 package com.maria.paginas_marcadas.entity;
 
-import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.maria.paginas_marcadas.entity.enums.StatusMemoriaLivro;
+import com.maria.paginas_marcadas.entity.enums.StatusMemoriaLeitura;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -27,12 +25,12 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "memoria_livro")
-public class MemoriaLivro {
+@Table(name = "memoria_leitura", schema = "principal")
+public class MemoriaLeitura {
 
-	@Id
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id_memoria_livro")
+    @Column(name = "id_memoria_leitura")
     private Long id;
 
     @Column(name = "titulo", length = 300)
@@ -44,23 +42,26 @@ public class MemoriaLivro {
     @Column(name = "descricao", columnDefinition = "TEXT")
     private String descricao;
 
-    @Column(name = "nota", precision = 3, scale = 1)
-    private BigDecimal nota;
+    @Column(name = "pagina_inicial")
+    private Integer paginaInicial;
 
-    @Column(name = "status_memoria_livro", length = 300)
-    private StatusMemoriaLivro status;
+    @Column(name = "pagina_final")
+    private Integer paginaFinal;
 
-    @Column(name = "data_cadastro")
+    @Column(name = "status_memoria_leitura", length = 300)
+    private StatusMemoriaLeitura status;
+
+    @Column(name = "data_cadastro", columnDefinition = "TIMESTAMP WITH TIME ZONE")
     private OffsetDateTime dataCadastro;
-
-    @ManyToMany(mappedBy = "memoriasLivro")
-    private List<Tag> tags = new ArrayList<>();
     
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne
     @JoinColumn(name = "id_leitura")
     private Leitura leitura;
-    
+
+    @ManyToMany(mappedBy = "memoriasLeitura")
+    private List<Tag> tags = new ArrayList<>();
+
     /*
-    @OneToMany(mappedBy = "memoriaLivro", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "memoriaLeitura", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Foto> fotos = new ArrayList<>();*/
 }
---
+++ src/main/java/com/maria/paginas_marcadas/entity/enums/StatusMemoriaLeitura.java
@@ -0,0 +1,10 @@
+package com.maria.paginas_marcadas.entity.enums;
+
+public enum StatusMemoriaLeitura {
+
+	EM_ANDAMENTO, 
+	PAUSADA, 
+	ABANDONADA, 
+	RETOMADA, 
+	FINALIZADA
+}
